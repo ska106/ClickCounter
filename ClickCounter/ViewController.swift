e376ca1091d1edea2c3fce03227c39fea8913d2a
Sudeep Agrawal <Sudeep.Agrawal@gmail.com>
2016-05-17 22:33:58 -0400

    Add the decrement button

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -23,19 +23,35 @@ class ViewController: UIViewController {
         self.view.addSubview(label)
         self.label = label
         
-        //Button
+        //Button - Click to Add
         let button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
+        button.frame = CGRectMake(150, 250, 100, 60)
+        button.setTitle("Click to Add", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.view.addSubview(button)
         
         button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Button - Click to Add
+        let button2 = UIButton()
+        button2.frame = CGRectMake(150, 320, 180, 60)
+        button2.setTitle("Click to Decrement", forState: .Normal)
+        button2.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button2)
+        
+        button2.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
 
     func incrementCount()
     {
         self.count += 1
+        self.label.text = "\(self.count)"
+    }
+    
+    func decrementCount()
+    {
+        self.count -= 1
         self.label.text = "\(self.count)"
     }
 }
